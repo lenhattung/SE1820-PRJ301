5770c5336e7515a1469c817b6ef26fac6fd8b287
Tung Le <lenhattung@gmail.com>
2025-01-20 11:37:41 +0700

    update
    update

--- WebApplication_05_a/src/java/utils/DBUtils.java
+++ WebApplication_05_a/src/java/utils/DBUtils.java
@@ -5,12 +5,37 @@
  */
 package utils;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author tungi
  */
 public class DBUtils {
     private static final String DB_NAME = "Web_05_a";
-    private static final String DB_USER_NAME = "sa";
+    private static final String DB_USERNAME = "sa";
     private static final String DB_PASSWORD= "12345";
+    
+    public static Connection getConnection() throws ClassNotFoundException, SQLException{
+        Connection conn = null;
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        String url = "jdbc:sqlserver://localhost:1433;databaseName="+DB_NAME;
+        conn = DriverManager.getConnection(url, DB_USERNAME, DB_PASSWORD);
+        return conn;
+    }
+    
+    public static void main(String[] args) {
+        try {
+            Connection conn = getConnection();
+            System.out.println(conn);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DBUtils.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(DBUtils.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
