c7452637e900e09ce64418d964a1b478cce8e224
Tung Le <lenhattung@gmail.com>
2025-02-06 11:10:46 +0700

    update
    update

--- WebApplication_06/src/java/dao/UserDAO.java
+++ WebApplication_06/src/java/dao/UserDAO.java
@@ -8,7 +8,13 @@ Giúp code dễ maintain và test hơn
 package dao;
 
 import dto.UserDTO;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import utils.DBUtils;
 
 /**
  *
@@ -18,7 +24,27 @@ public class UserDAO implements IDAO<UserDTO, String>{
 
     @Override
     public boolean create(UserDTO entity) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = "INSERT [dbo].[tblUsers] ([userID], [fullName], [roleID], [password]) "
+                + "VALUES (N'"+entity.getUserID()
+                +"', N'"+entity.getFullName()
+                +"', N'"+entity.getRoleID()
+                +"', N'"+entity.getPassword()+"')";
+        try {
+            Connection conn = DBUtils.getConnection();
+            Statement st = conn.createStatement();
+            int n = st.executeUpdate(sql);
+//            if(n>0){
+//                return true;
+//            }else{
+//                return false;
+//            }
+            return n>0;
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
     }
 
     @Override
---
+++ WebApplication_06/src/java/test/UserTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import dao.UserDAO;
+import dto.UserDTO;
+
+/**
+ *
+ * @author tungi
+ */
+public class UserTest {
+    public static void main(String[] args) {
+        UserDAO userDAO = new UserDAO();
+//        UserDTO ud1 = new UserDTO("TLN01", "Le Nhat Tung", "AD", "khong_co_password");
+//        userDAO.create(ud1);
+        for (int i = 0; i < 10; i++) {
+            UserDTO ud_i = new UserDTO("USER"+i, "Nguyen Van "+i, "US", "_"+i);
+            userDAO.create(ud_i);
+        }
+    }
+}
