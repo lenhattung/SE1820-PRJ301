1e57aee83a531c1f1896803a2ad1b05168f557fa
Tung Le <lenhattung@gmail.com>
2025-02-06 10:50:54 +0700

    update
    update

---
+++ WebApplication_06/src/java/dao/UserDAO.java
@@ -0,0 +1,49 @@
+/*
+DAO là một design pattern dùng để truy cập và thao tác với database
+Chứa tất cả các phương thức CRUD (Create, Read, Update, Delete)
+Đóng gói toàn bộ logic truy cập database
+Tách biệt logic truy cập dữ liệu khỏi business logic
+Giúp code dễ maintain và test hơn
+ */
+package dao;
+
+import dto.UserDTO;
+import java.util.List;
+
+/**
+ *
+ * @author tungi
+ */
+public class UserDAO implements IDAO<UserDTO, String>{
+
+    @Override
+    public boolean create(UserDTO entity) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<UserDTO> readAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public UserDTO readById(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean update(UserDTO entity) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean delete(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<UserDTO> search(String searchTerm) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
--- WebApplication_06/src/java/dto/UserDTO.java
+++ WebApplication_06/src/java/dto/UserDTO.java
@@ -1,6 +1,9 @@
 /*
 DTO (Data Transfer Object):
-
+DTO là một design pattern dùng để đóng gói và truyền dữ liệu giữa các thành phần/layer trong ứng dụng
+Là một class đơn giản chỉ chứa các thuộc tính (properties) và các phương thức getter/setter
+Không chứa bất kỳ business logic nào
+Mục đích chính là đóng gói dữ liệu để truyền qua network hoặc giữa các layer
  */
 package dto;
 
