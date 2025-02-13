aa0e80f0528961a540a333db57d1def9db52ab4f
Tung Le <lenhattung@gmail.com>
2025-02-13 11:40:46 +0700

    update
    update

--- WebApplication_08/src/java/controller/MainController.java
+++ WebApplication_08/src/java/controller/MainController.java
@@ -5,8 +5,11 @@
  */
 package controller;
 
+import dao.UserDAO;
+import dto.UserDTO;
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -20,19 +23,55 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "MainController", urlPatterns = {"/MainController"})
 public class MainController extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    private static final String LOGIN_PAGE = "login.jsp";
+
+    public UserDTO getUser(String strUserID) {
+        UserDAO udao = new UserDAO();
+        UserDTO user = udao.readById(strUserID);
+        return user;
+    }
+
+    public boolean isValidLogin(String strUserID, String strPassword) {
+        UserDTO user = getUser(strUserID);
+//        if(user!=null && user.getPassword().equals(strPassword)){
+//            return true;
+//        }else{
+//            return false;
+//        }
+
+        System.out.println(user);
+        System.out.println(user.getPassword());
+        System.out.println(strPassword);
+        return user != null && user.getPassword().equals(strPassword);
+    }
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        
+        String url = LOGIN_PAGE;
+        try {
+            String action = request.getParameter("action");
+            System.out.println(action);
+            if (action == null) {
+                url = LOGIN_PAGE;
+            } else {
+                if (action.equals("login")) {
+                    String strUserID = request.getParameter("txtUserID");
+                    String strPassword = request.getParameter("txtPassword");
+                    if(isValidLogin(strUserID, strPassword)){
+                        url ="search.jsp";
+                    }else{
+                        url ="invalid.jsp";
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            log("Error at MainController: " + e.toString());
+        } finally {
+            RequestDispatcher rd = request.getRequestDispatcher(url);
+            rd.forward(request, response);
+        }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
